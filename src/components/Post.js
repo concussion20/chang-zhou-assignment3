68a49b4d0e9e52d1f49ff90f2697f6bdb5d50cdb
concussion20 <zhou.chang1@husky.neu.edu>
2021-04-28 08:24:38 -0700

    when post deleted, return to home page

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,6 +13,7 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 import PostTitle from './PostTitle';
 import Comment from './Comment';
+import {redirect, getLoggedinUsername} from './Loggedin';
 
 export default function Post() {
   const { postId } = useParams();
@@ -21,8 +22,9 @@ export default function Post() {
   const [formError, setFormError] = useState(false);
   const [publishOK, setPublishOK] = useState("req not send");
 
-  const cookies = new Cookies();
-  let username = cookies.get('username');
+  // const cookies = new Cookies();
+  // let username = cookies.get('username');
+  let username = getLoggedinUsername();
 
   let commentRef;
   let commentElements;
